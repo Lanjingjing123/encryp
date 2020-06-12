48915d36e8cca0c85b9f6dee434586b7ccb56743
lanjingjing <1792539263@qq.com>
2020-06-12 09:59:20 +0800

    增加 AES/CBC/ZeroPadding  AES/ECB/PKSC5Padding 两种模式的demo

--- src/main/java/com/csii/ljj/AESEncrypt.java
+++ src/main/java/com/csii/ljj/AESEncrypt.java
@@ -1,9 +1,16 @@
 package com.csii.ljj;
 
 import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * AES加密，CBC模式，zeropadding填充，偏移量为key
@@ -13,80 +20,111 @@ public class AESEncrypt {
     private static String ivs = "Ym888pPr888pc888";
 
     private static String key = "3Cdp488899rC888T";
-    //加密
-    public static String encrypt(String data) {
+
+
+    /**
+     * 加密
+     * @since  算法/模式/补码方式:AES/CBC/ZeroPadding
+     * @param data
+     * @return 加密的数据
+     * @doc  ZeroPadding:不足16位的整数倍补0
+     */
+    public static String encrypt_CBC_ZeroPadding(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         String ivString = ivs;
         //偏移量
         byte[] iv = ivString.getBytes();
-        try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-            int blockSize = cipher.getBlockSize();
-            byte[] dataBytes = data.getBytes();
-            int length = dataBytes.length;
-            //计算需填充长度
-            if (length % blockSize != 0) {
-                length = length + (blockSize - (length % blockSize));
-            }
-            byte[] plaintext = new byte[length];
-            //填充
-            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
-            for (byte b:plaintext) {
-                System.out.println(b);
-            }
-
-            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
-            //设置偏移量参数
-            IvParameterSpec ivSpec = new IvParameterSpec(iv);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-            byte[] encryped = cipher.doFinal(plaintext);
-
-            return Base64.encodeBase64String(encryped);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        int blockSize = cipher.getBlockSize();
+        byte[] dataBytes = data.getBytes();
+        int length = dataBytes.length;
+        //计算需填充长度
+        if (length % blockSize != 0) {
+            length = length + (blockSize - (length % blockSize));
         }
+        byte[] plaintext = new byte[length];
+        //填充
+        System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        //设置偏移量参数
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        byte[] encryped = cipher.doFinal(plaintext);
+        return Base64.encodeBase64String(encryped).replaceAll("[\\s*\t\n\r]", "");
     }
 
-    //解密
-    public static String desEncrypt(String data) {
 
-        String ivString = ivs;
-        byte[] iv = ivString.getBytes();
+    /**
+     * 解密
+     * @since 算法/模式/补码方式:AES/CBC/ZeroPadding
+     * @param data
+     * @return
+     */
+    public static String dencrypt_CBC_ZeroPadding(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
+        byte[] iv = ivs.getBytes();
 
-        try {
-            byte[] encryp = Base64.decodeBase64(data);
+        byte[] encryp = Base64.decodeBase64(data);
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
-            IvParameterSpec ivSpec = new IvParameterSpec(iv);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-            byte[] original = cipher.doFinal(encryp);
-            return new String(original);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        byte[] original = cipher.doFinal(encryp);
+
+        return new String(original).trim();// 解密之后去除加密时补的0（空字符串）
+    }
+    /**
+     * @since  使用对称密钥进行加密  算法/模式/不骂方式:AES/ECB/PKCS5Padding
+     * @param key  genKeyString()中生成的key字符
+     * @param oriContent    原文（UTF-8编码）
+     * @return
+     * @throws Exception
+     * 注：默认UTF-8编码，加密之后用BASE64进行转码
+     */
+    public static String  encrpt_ECB_PKCS5Padding(String key,String oriContent) throws Exception{
+
+        byte[] keyb = Base64.decodeBase64(key);// 秘钥进行Base64 解码
+        SecretKeySpec sKeySpec = new SecretKeySpec(keyb, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);// ECB 模式不需要设置iv
+        byte[] encrptBytes = cipher.doFinal(oriContent.getBytes("UTF-8"));
+        String encreptContent = Base64.encodeBase64String(encrptBytes);
+        encreptContent = encreptContent.replaceAll("[\\s*\t\n\r]", "");
+        return encreptContent;
+    }
+    /**
+     * 使用对称密钥进行解密
+     * @param key genKeyString()中生成的key字节数组
+     * @param encrptContent 经过Base64转码之后的密文
+     * @throws Exception
+     */
+    public static String decrpt_ECB_PKCS5Padding(String key,String encrptContent) throws Exception{
+
+        byte[] encrptBytes = Base64.decodeBase64(encrptContent);// 先将密文解码
+        byte[] keyb = Base64.decodeBase64(key);// key使用Base64 进行转码
+        SecretKeySpec sKeySpec = new SecretKeySpec(keyb, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, sKeySpec);// ECB模式不使用iv
+
+        byte[] decrptContent = cipher.doFinal(encrptBytes);
+       return new String(decrptContent);
     }
     //测试
-    public static void main(String[] args) {
-        String data = "hello world ~,你好，中国";
-        String key = "4385095701807607";
-        String encrypt = encrypt(data);
-        String desencrypt = desEncrypt(encrypt);
+    public static void main(String[] args) throws Exception {
+        // AES/CBC/ZeroPadding
+        String data = "hello world ~,你好，中国发骄傲了房间里撒娇砥砺奋进爱睡懒觉按实际发的垃圾四六级卡视角东方丽景爱上了就发啦老实交代冷风机案例所肩负的两三放两三点冷风机三街坊邻居了就两块三分段了就撒里的解放路卡机是两地分居";
+
+        String encrypt = encrypt_CBC_ZeroPadding(data);
+        String desencrypt =dencrypt_CBC_ZeroPadding (encrypt);
         System.out.println("加密后:"+encrypt);
         System.out.println("解密后:"+desencrypt);
 
+//        // AES/ECB/PKCS5Padding
+        String ikey = AESUtil.genKeyString();
 
-        byte[] arr = new byte[10];
-
-        arr[0] = 64;
-        arr[1] = 54;
-        arr[2] = 0x00;
-        for (Byte item:arr ) {
-            System.out.println(item);
-        }
-
+        String encrptContent = encrpt_ECB_PKCS5Padding(ikey, data);
+        System.out.println("AES-加密的数据："+encrptContent);
+        String decrptContent = decrpt_ECB_PKCS5Padding(ikey, encrptContent);
+        System.out.println("AES-解密的数据："+decrptContent);
 
     }
 }
--- src/main/java/com/csii/ljj/AESUtil.java
+++ src/main/java/com/csii/ljj/AESUtil.java
@@ -6,6 +6,7 @@ import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -17,44 +18,8 @@ import java.security.SecureRandom;
  */
 public class AESUtil {
 
-    private static String ivs = "0392039203920300";
-
-
-    /**
-     * 二进制byte[]转十六进制string
-     */
-    public static String byteToHexString(byte[] bytes){
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < bytes.length; i++) {
-            String strHex=Integer.toHexString(bytes[i]);
-            if(strHex.length() > 3){
-                sb.append(strHex.substring(6));
-            } else {
-                if(strHex.length() < 2){
-                    sb.append("0" + strHex);
-                } else {
-                    sb.append(strHex);
-                }
-            }
-        }
-        return  sb.toString();
-    }
-
-    /**
-     * 十六进制string转二进制byte[]
-     */
-    public static byte[] hexStringToByte(String s) {
-        byte[] baKeyword = new byte[s.length() / 2];
-        for (int i = 0; i < baKeyword.length; i++) {
-            try {
-                baKeyword[i] = (byte) (0xff & Integer.parseInt(s.substring(i * 2, i * 2 + 2), 16));
-            } catch (Exception e) {
-                System.out.println("十六进制转byte发生错误！！！");// TODO
-                e.printStackTrace();
-            }
-        }
-        return baKeyword;
-    }
+    private static String ivs = "Ym888pPr888pc888";
+    private static String key = "3Cdp488899rC888T";
 
     /**
      * 随机生成AES128位密钥字符串
@@ -183,6 +148,93 @@ public class AESUtil {
         byte[] decrptContent = cipher.doFinal(encrptBytes);
         System.out.println("Decrpt:"+new String(decrptContent));
     }
+
+    /**
+     * 加密
+     * @since  算法/模式/补码方式:AES/CBC/ZeroPadding
+     * @param data
+     * @return 加密的数据
+     * @doc  ZeroPadding:不足16位的整数倍补0
+     */
+    public static String encrypt_CBC_ZeroPadding(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        String ivString = ivs;
+        //偏移量
+        byte[] iv = ivString.getBytes();
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        int blockSize = cipher.getBlockSize();
+        byte[] dataBytes = data.getBytes();
+        int length = dataBytes.length;
+        //计算需填充长度
+        if (length % blockSize != 0) {
+            length = length + (blockSize - (length % blockSize));
+        }
+        byte[] plaintext = new byte[length];
+        //填充
+        System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        //设置偏移量参数
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        byte[] encryped = cipher.doFinal(plaintext);
+        return Base64.encodeBase64String(encryped).replaceAll("[\\s*\t\n\r]", "");
+    }
+
+
+    /**
+     * 解密
+     * @since 算法/模式/补码方式:AES/CBC/ZeroPadding
+     * @param data
+     * @return
+     */
+    public static String dencrypt_CBC_ZeroPadding(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
+        byte[] iv = ivs.getBytes();
+
+        byte[] encryp = Base64.decodeBase64(data);
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        byte[] original = cipher.doFinal(encryp);
+
+        return new String(original).trim();// 解密之后去除加密时补的0（空字符串）
+    }
+    /**
+     * @since  使用对称密钥进行加密  算法/模式/不骂方式:AES/ECB/PKCS5Padding
+     * @param key  genKeyString()中生成的key字符
+     * @param oriContent    原文（UTF-8编码）
+     * @return
+     * @throws Exception
+     * 注：默认UTF-8编码，加密之后用BASE64进行转码
+     */
+    public static String  encrpt_ECB_PKCS5Padding(String key,String oriContent) throws Exception{
+
+        byte[] keyb = Base64.decodeBase64(key);// 秘钥进行Base64 解码
+        SecretKeySpec sKeySpec = new SecretKeySpec(keyb, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);// ECB 模式不需要设置iv
+        byte[] encrptBytes = cipher.doFinal(oriContent.getBytes("UTF-8"));
+        String encreptContent = Base64.encodeBase64String(encrptBytes);
+        encreptContent = encreptContent.replaceAll("[\\s*\t\n\r]", "");
+        return encreptContent;
+    }
+    /**
+     * 使用对称密钥进行解密
+     * @param key genKeyString()中生成的key字节数组
+     * @param encrptContent 经过Base64转码之后的密文
+     * @throws Exception
+     */
+    public static String decrpt_ECB_PKCS5Padding(String key,String encrptContent) throws Exception{
+
+        byte[] encrptBytes = Base64.decodeBase64(encrptContent);// 先将密文解码
+        byte[] keyb = Base64.decodeBase64(key);// key使用Base64 进行转码
+        SecretKeySpec sKeySpec = new SecretKeySpec(keyb, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, sKeySpec);// ECB模式不使用iv
+
+        byte[] decrptContent = cipher.doFinal(encrptBytes);
+        return new String(decrptContent);
+    }
     public static void main(String[] args) throws Exception {
 
         String oriContent = "hello world! 你好世界";
@@ -197,6 +249,25 @@ public class AESUtil {
         String encrptContent2 = encrpt(key, oriContent);
         System.out.println("encrpt2:"+encrptContent2);
         decrpt(key,encrptContent2);
+
+
+
+        // AES/CBC/ZeroPadding
+        String data = "hello world ~,你好，中国发骄傲了房间里撒娇砥砺奋进爱睡懒觉按实际发的垃圾四六级卡视角东方丽景爱上了就发啦老实交代冷风机案例所肩负的两三放两三点冷风机三街坊邻居了就两块三分段了就撒里的解放路卡机是两地分居";
+
+        String encrypt = encrypt_CBC_ZeroPadding(data);
+        String desencrypt =dencrypt_CBC_ZeroPadding (encrypt);
+        System.out.println("加密后:"+encrypt);
+        System.out.println("解密后:"+desencrypt);
+
+//        // AES/ECB/PKCS5Padding
+        String ikey = AESUtil.genKeyString();
+
+        String encrptContent3 = encrpt_ECB_PKCS5Padding(ikey, data);
+        System.out.println("AES-加密的数据："+encrptContent3);
+        String decrptContent = decrpt_ECB_PKCS5Padding(ikey, encrptContent3);
+        System.out.println("AES-解密的数据："+decrptContent);
+
 
 
     }
